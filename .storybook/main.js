87df5eb747fe26e570128cd0bee38f31047ca76f
Yuta Matsuo <y.matsuo.dev@gmail.com>
2024-02-22 09:03:21 +0900

    chore: upgrade storybook -> 7.6.17

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,18 +3,23 @@ const path = require('path');
 module.exports = {
   typescript: { reactDocgen: false },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/preset-scss',
     '@storybook/addon-viewport',
+    '@storybook/addon-mdx-gfm'
   ],
+
   staticDirs: ['../public'],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
+
   webpackFinal: async (config) => {
     // エイリアス設定
     config.resolve.alias = {
@@ -56,4 +61,8 @@ module.exports = {
 
     return config;
   },
+
+  docs: {
+    autodocs: true
+  }
 };
